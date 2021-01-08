6c80659ea52364a6e38be42734b331a63aad2e15
Logan McAnsh <logan@mcan.sh>
2021-01-07 21:06:02 -0500

    chore: add an (ugly) alert when using the app without javascript
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/app.tsx
+++ app/app.tsx
@@ -14,6 +14,13 @@ function App() {
       </head>
       <body>
         {data.flash && <pre>{data.flash}</pre>}
+        <noscript>
+          <div className="fixed top-0 left-0 p-2 text-white bg-pink-500 sm:rounded-lg sm:top-2 sm:left-2 sm:max-w-md">
+            While this app will technically work without javascript, you'll have
+            a happier time if you enable it 😃
+          </div>
+          <div className="mt-20" />
+        </noscript>
         <Routes />
         <Scripts />
       </body>
