b94c073c703429d86a446ec7e8a562fdb1d27c23
Logan McAnsh <logan@mcan.sh>
2021-01-11 22:22:49 -0500

    chore: include custom ProcessEnv types in server, x-ref 5ac4006
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="../app/@types/global.d.ts" />
+
 import path from 'path';
 
 import express from 'express';
