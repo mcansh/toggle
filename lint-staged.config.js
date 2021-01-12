aa981551732df22993af02ca9cd0669dafee5b2c
Logan McAnsh <logan@mcan.sh>
2021-01-12 17:00:02 -0500

    chore: update lint staged
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,23 +1,31 @@
 const { quote: escape } = require('shell-quote');
+const { CLIEngine } = require('eslint');
+
+const cli = new CLIEngine({});
 const isWin = process.platform === 'win32';
 
 module.exports = {
-  '*.+(js|jsx|ts|tsx)': filenames => {
-    const escapedFileNames = isWin ? filenames : escape(filenames);
-    const filenamesForESLint = filenames.join(' ');
+  '**/*.{js,jsx,ts,tsx}': filenames => {
+    const escapedFileNames = filenames
+      .map(filename => `"${isWin ? filename : escape([filename])}"`)
+      .join(' ');
+
+    const filesForEslint = filenames
+      .filter(file => !cli.isPathIgnored(file))
+      .map(f => `"${f}"`)
+      .join(' ');
 
     return [
       `prettier --write ${escapedFileNames}`,
-      `eslint --cache --fix --quiet ${filenamesForESLint}`,
+      `eslint --cache --fix --quiet ${filesForEslint}`,
     ];
   },
 
-  '*.+(json|yml|yaml|css|less|scss|md|graphql|mdx)': filenames => {
-    const escapedFileNames = isWin ? filenames : escape(filenames);
+  '*.+(json,yml,yaml,htm,css,less,scss,md,graphql,mdx)': filenames => {
+    const escapedFileNames = filenames
+      .map(filename => `"${isWin ? filename : escape([filename])}"`)
+      .join(' ');
 
-    return [
-      //
-      `prettier --write ${escapedFileNames}`,
-    ];
+    return [`prettier --write ${escapedFileNames}`];
   },
 };
