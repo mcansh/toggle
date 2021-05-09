5e338654d55c0751f577c066e79129c5f6a25ad2
Logan McAnsh <logan@mcan.sh>
2021-05-09 16:58:20 -0400

    chore(LoginPage,JoinPage): add flag icon
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/routes/join.tsx
+++ app/routes/join.tsx
@@ -5,6 +5,7 @@ import slugify from 'slugify';
 import { ValidationError } from 'yup';
 import { json } from 'remix-utils';
 import clsx from 'clsx';
+import { FlagIcon } from '@heroicons/react/outline';
 
 import { withSession } from '../lib/with-session';
 import { generateName } from '../lib/name-generator';
@@ -127,11 +128,7 @@ function JoinPage() {
   return (
     <div className="flex flex-col justify-center min-h-screen py-12 bg-gray-50 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <img
-          className="w-auto h-12 mx-auto"
-          src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-          alt="Workflow"
-        />
+        <FlagIcon className="w-auto h-12 mx-auto text-indigo-600" />
         <h2 className="mt-6 text-3xl font-extrabold text-center text-gray-900">
           Join Toggle
         </h2>
--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Form, Link, usePendingFormSubmit, redirect } from 'remix';
 import type { ActionFunction, LoaderFunction } from 'remix';
+import { FlagIcon } from '@heroicons/react/outline';
 
 import { flashTypes } from '../lib/flash';
 import { withSession } from '../lib/with-session';
@@ -85,11 +86,7 @@ function LoginPage() {
   return (
     <div className="flex flex-col justify-center min-h-screen py-12 bg-gray-50 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <img
-          className="w-auto h-12 mx-auto"
-          src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-          alt="Workflow"
-        />
+        <FlagIcon className="w-auto h-12 mx-auto text-indigo-600" />
         <h2 className="mt-6 text-3xl font-extrabold text-center text-gray-900">
           Sign in to Toggle
         </h2>
